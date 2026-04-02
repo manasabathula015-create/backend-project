fbcbde33860e2c3d454fa12d67e6111dc9996f68
manasabathula015-create <manasabathula015@gmail.com>
2026-04-02 17:30:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,6 @@ def download():
 
     return send_file(file_path, as_attachment=True)
 
-app.run(port=5000)
+if __name__ == "__main__":
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
