6b4f80e03099a7195ab803f1a0219757f31f061b
manasabathula015-create <manasabathula015@gmail.com>
2026-04-02 12:10:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import csv
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+total = 0
+today_total = 0
+records = []
+
+@app.route('/')
+def home():
+    return "Server is running"
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    global total, today_total, records
+
+    data = request.json
+    name = data.get('name')
+    mobile = data.get('mobile')
+    count = data.get('count')
+
+    if not name or not mobile or not count:
+        return jsonify({"error": "All fields required"}), 400
+
+    total += int(count)
+    today_total += int(count)
+
+    records.append({
+        "name": name,
+        "mobile": mobile,
+        "count": count
+    })
+
+    return jsonify({
+        "totalCount": total,
+        "todayCount": today_total,
+        "individualCount": count
+    })
+
+@app.route('/download')
+def download():
+    file_path = os.path.join(os.getcwd(), "report.csv")
+
+    with open(file_path, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Name", "Mobile", "Count"])
+
+        for r in records:
+            writer.writerow([r["name"], r["mobile"], r["count"]])
+
+    return send_file(file_path, as_attachment=True)
+
+app.run(port=5000)
